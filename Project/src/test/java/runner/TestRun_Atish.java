b5a0d96c4ce7f0b7cc2192d9ad1bc30b6afe7f41
atish-2025 <atish.ltimindtree@gmail.com>
2025-04-14 06:05:48 +0000

    Initial commit

--- Project/src/test/java/runner/TestRun_Atish.java
+++ Project/src/test/java/runner/TestRun_Atish.java
@@ -7,6 +7,6 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "./features", glue = "stepdefinition")
-public class TestRun {
+public class TestRun_Atish {
 
 }
